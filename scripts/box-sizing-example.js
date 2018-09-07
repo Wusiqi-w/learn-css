94888c984cded291b663c23842cacce25e3773e4
Wusiqi-w <Wusiqi_wm@163.com>
2018-09-07 17:18:12 +0800

    样式化区块

---
+++ scripts/box-sizing-example.js
@@ -0,0 +1,12 @@
+var one = document.querySelector('.one');
+var two = document.querySelector('.two');
+
+
+function outputWH(box) {
+    var width = box.offsetWidth;
+    var height = box.offsetHeight;
+    box.textContent = 'Width: '+ width + 'px, Height' + height + 'px.';
+}
+
+outputWH(one);
+outputWH(two);
